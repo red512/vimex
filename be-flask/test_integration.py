dacee603f4562b1001461ebe80b8146401fb5f52
red512 <robojinx1@gmail.com>
2024-03-07 19:56:54 +0200

    fixed the tests

--- be-flask/test_integration.py
+++ be-flask/test_integration.py
@@ -6,7 +6,7 @@ def test_home_route():
     response = requests.get('http://localhost:5000/')
     assert response.status_code == 200
     assert b'"message":"Success"' in response.content
-    assert b'"weather":"New York"' in response.content
+    assert b'"city":"New York"' in response.content
     print("integration tests passed")
 
 test_home_route()
